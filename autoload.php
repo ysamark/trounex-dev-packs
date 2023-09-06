7a1d19c50dc1b5c6ddabf6aa9de0df0889aa68b5
agostinhosamil <agostinhosaml832@gmail.com>
2023-09-06 15:03:00 +0100

    feat(php\module/config): config php_modules

--- autoload.php
+++ autoload.php
@@ -1,1 +1,14 @@
 <?php
+
+use php\module;
+use Trounex\Development\Trounex;
+
+$applicationRootDir = Trounex::GetApplicationRootDir ();
+
+if (class_exists (module::class)) {
+  module::config ([
+    'rootDir' => $applicationRootDir
+  ]);
+
+  module::initialize_config ();
+}
