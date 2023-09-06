c1efdb2ed543cff300d9211865be38c26d68516f
agostinhosamil <agostinhosaml832@gmail.com>
2023-09-06 15:02:23 +0100

    feat: create getAppRootDir helper

--- Trounex/Development/Trounex.php
+++ Trounex/Development/Trounex.php
@@ -1,1 +1,29 @@
 <?php
+
+namespace Trounex\Development;
+
+class Trounex {
+  /**
+   * Get the main application root dir
+   */
+  public static function getApplicationRootDir () {
+    $isRootDir = function ($dir) {
+      return (boolean)(
+        is_file ($dir . '/composer.json') &&
+        is_dir ($dir . '/vendor/ysamark/trounex-dev-packs/Trounex/Development')
+      );
+    };
+
+    $currentDir = dirname (__DIR__);
+
+    $rootDirFetchIntervalCount = count (preg_split ('/(\/|\\\\)/', $currentDir));
+
+    for ( ; $rootDirFetchIntervalCount >= 0; $rootDirFetchIntervalCount--) {
+      if (call_user_func_array ($isRootDir, [$currentDir])) {
+        return $currentDir;
+      }
+
+      $currentDir = dirname ($currentDir);
+    }
+  }
+}
