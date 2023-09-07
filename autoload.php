4c0b1d5fe7337b1bc877364318c5357da62b4271
agostinhosamil <agostinhosaml832@gmail.com>
2023-09-07 09:06:07 +0100

    feat(config): remove php module config form autoload file

--- autoload.php
+++ autoload.php
@@ -1,14 +1,1 @@
 <?php
-
-use php\module;
-use Trounex\Development\Trounex;
-
-$applicationRootDir = Trounex::GetApplicationRootDir ();
-
-if (class_exists (module::class)) {
-  module::config ([
-    'rootDir' => $applicationRootDir
-  ]);
-
-  module::initialize_config ();
-}
